c8a4981972876fb2b5035def0fff09aaa8b3497f
Shamaz saeed <shamazsaeed332@gmail.com>
2022-07-09 20:25:26 +0500

    init

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../Header/Header'
+import './Hero.css'
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className='left-h'>
+        <Header/>
+      </div>
+      <div className='right-h'></div>
+    </div>
+  )
+}
+
+export default Hero
